8fa5e9ca2feed25ccba6edf099616ab7c6f2e64a
Tonius <toniuswar@gmail.com>
2023-09-13 23:05:49 +0300

    Images urls edited

--- routes/destructive_elements.js
+++ routes/destructive_elements.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const elements = { 
-    samples: [{
+    samples: [
+        {
         id:1,
         title: "Tornado_1",
-        img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_1.png"
+        img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_1.png?raw=true"
     },
     {
         id:2,
         title: "Tornado_2",
-        img:"https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_2.png"
+        img:"https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_2.png?raw=true"
     },
     {
         id:3,
         title: "Tornado_3",
-        img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_3.png"
-    }]
+        img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_3.png?raw=true"
+    }
+]
 }
 
 router.get("/destructive_element", (req, res)=>{
