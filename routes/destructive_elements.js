72508779aeb1238db8336ba3dcc88c84eddbaae5
Tonius <toniuswar@gmail.com>
2023-09-13 21:57:03 +0300

    Elements edited

--- routes/destructive_elements.js
+++ routes/destructive_elements.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const elements = [
-    {
+const elements = { 
+    samples: [{
         id:1,
         title: "Tornado_1",
         img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_1.png"
@@ -15,8 +15,8 @@ const elements = [
         id:3,
         title: "Tornado_3",
         img: "https://github.com/toniwar/Extreme_Camera_Server/blob/master/images/tornado_3.png"
-    }
-]
+    }]
+}
 
 router.get("/destructive_element", (req, res)=>{
     res.send(elements);
